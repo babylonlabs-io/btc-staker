d2be612891a4a351de63643886951555d87b01bb
KonradStaniec <konrad.staniec@gmail.com>
2025-01-13 12:56:00 +0100

    Konradstaniec/delete endpoint and docs (#115)
    * refactor sign adr36 to separate function
    
    * add command to delete pop payload

--- staker/pop_creator.go
+++ staker/pop_creator.go
@@ -42,59 +42,6 @@ func NewPopCreator(bitcoinWalletController *walletcontroller.RPCWalletController
 	}
 }
 
-func (pc *PopCreator) getBabyPubKey(babylonAddress sdk.AccAddress) (*keyring.Record, *secp256k1.PubKey, error) {
-	record, err := pc.KeyRing.KeyByAddress(babylonAddress)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	pubKey, err := record.GetPubKey()
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	switch v := pubKey.(type) {
-	case *secp256k1.PubKey:
-		return record, v, nil
-	default:
-		return nil, nil, fmt.Errorf("unsupported key type in keyring")
-	}
-}
-
-func (pc *PopCreator) signCosmosAdr36(
-	keyName string,
-	cosmosBech32Address string,
-	bytesToSign []byte,
-) ([]byte, error) {
-	base64Bytes := base64.StdEncoding.EncodeToString(bytesToSign)
-
-	signDoc := NewCosmosSignDoc(
-		cosmosBech32Address,
-		base64Bytes,
-	)
-
-	marshaled, err := json.Marshal(signDoc)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal sign doc: %w", err)
-	}
-
-	bz := sdk.MustSortJSON(marshaled)
-
-	babySignBTCAddress, _, err := pc.KeyRing.Sign(
-		keyName,
-		bz,
-		signing.SignMode_SIGN_MODE_DIRECT,
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to sign btc address bytes: %w", err)
-	}
-
-	return babySignBTCAddress, nil
-}
-
 func (pc *PopCreator) CreatePop(
 	btcAddress btcutil.Address,
 	babyAddressPrefix string,
@@ -124,12 +71,13 @@ func (pc *PopCreator) CreatePop(
 		return nil, err
 	}
 
-	record, babyPubKey, err := pc.getBabyPubKey(babyAddress)
+	record, babyPubKey, err := GetBabyPubKey(pc.KeyRing, babyAddress)
 	if err != nil {
 		return nil, err
 	}
 
-	babySignBTCAddress, err := pc.signCosmosAdr36(
+	babySignBTCAddress, err := SignCosmosAdr36(
+		pc.KeyRing,
 		record.Name,
 		bech32cosmosAddressString,
 		[]byte(btcAddress.String()),
@@ -196,4 +144,58 @@ func NewCosmosSignDoc(
 		},
 		Memo: "",
 	}
+}
+
+func GetBabyPubKey(kr keyring.Keyring, babylonAddress sdk.AccAddress) (*keyring.Record, *secp256k1.PubKey, error) {
+	record, err := kr.KeyByAddress(babylonAddress)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pubKey, err := record.GetPubKey()
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	switch v := pubKey.(type) {
+	case *secp256k1.PubKey:
+		return record, v, nil
+	default:
+		return nil, nil, fmt.Errorf("unsupported key type in keyring")
+	}
+}
+
+func SignCosmosAdr36(
+	kr keyring.Keyring,
+	keyName string,
+	cosmosBech32Address string,
+	bytesToSign []byte,
+) ([]byte, error) {
+	base64Bytes := base64.StdEncoding.EncodeToString(bytesToSign)
+
+	signDoc := NewCosmosSignDoc(
+		cosmosBech32Address,
+		base64Bytes,
+	)
+
+	marshaled, err := json.Marshal(signDoc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal sign doc: %w", err)
+	}
+
+	bz := sdk.MustSortJSON(marshaled)
+
+	babySignBTCAddress, _, err := kr.Sign(
+		keyName,
+		bz,
+		signing.SignMode_SIGN_MODE_DIRECT,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign btc address bytes: %w", err)
+	}
+
+	return babySignBTCAddress, nil
 }
